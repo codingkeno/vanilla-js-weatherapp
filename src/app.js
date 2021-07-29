abdb82684a4bfe0c74542777ef71e5da71e064db
codingkeno <66284009+codingkeno@users.noreply.github.com>
2021-07-28 22:14:48 -0400

    Formatted date and time from API

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let lastUpdate = new Date(timestamp);
+  let hours = lastUpdate.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = lastUpdate.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[lastUpdate.getDay()];
+  return `${day} at ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -10,6 +33,8 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "36b04148136c5da7b52a56c68d439cee";
